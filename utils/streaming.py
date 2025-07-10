1ae568b45d5322e002e4266ea31a582d50c04892
Habib-bhai <habibahmed918131@gmail.com>
2025-07-10 14:00:37 +0500

    agent update stream handle

--- utils/streaming.py
+++ utils/streaming.py
@@ -2,7 +2,7 @@ from agents import Runner
 from context import UserSessionContext
 from agent import Health_wellness_agent
 from openai.types.responses import ResponseTextDeltaEvent
-
+import json
 
 
 
@@ -18,6 +18,17 @@ async def main_streaming(user_input : str):
 
     async for event in result.stream_events():
         if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
-            yield event.data.delta
+              yield json.dumps({
+                "type": "text",
+                "content": event.data.delta
+            }) + "\n"
+        
+        elif event.type == "agent_updated_stream_event":
+            # Adjust based on your actual event structure
+            yield json.dumps({
+                "type": "agent_handoff",
+                "new_agent": event.new_agent.name,
+                "message": f"Transferring to {event.new_agent.name} specialist..."
+            }) + "\n"
             
     
